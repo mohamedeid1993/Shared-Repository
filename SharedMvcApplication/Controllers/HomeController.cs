006fc0d5b3a4140974cff6042ca4cca79d674958
mohamedeid1993 <38851359+mohamedeid1993@users.noreply.github.com>
2018-07-26 08:54:13 +0200

    Update HomeController.cs

--- SharedMvcApplication/Controllers/HomeController.cs
+++ SharedMvcApplication/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SharedMvcApplication.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-            ViewBag.Message = "Modified Message";
+            ViewBag.Message = "Modified Message on server";
 
             return View();
         }
